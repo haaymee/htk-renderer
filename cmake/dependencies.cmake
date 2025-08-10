41078281ba10ed7011c0af834562b285d1389789
Jaime Martin <workjaimemartin@gmail.com>
2025-08-10 16:17:54 +0800

    feat : integrated SDL into project

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -8,10 +8,36 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/g-truc/glm.git
     GIT_TAG 0af55cc
     GIT_SHALLOW TRUE
+
+    SOURCE_DIR ${FETCHCONTENT_BASE_DIR}/glm/source
+    BINARY_DIR ${FETCHCONTENT_BASE_DIR}/glm/bin
 )
 
-FetchContent_MakeAvailable(glm)
+FetchContent_Declare(
+    SDL3
+    GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
+    GIT_TAG 96292a5
+    GIT_SHALLOW TRUE
 
-if(TARGET glm)
-    set_target_properties(glm PROPERTIES FOLDER "Third Party")
-endif()
+    SOURCE_DIR ${FETCHCONTENT_BASE_DIR}/SDL/source
+    BINARY_DIR ${FETCHCONTENT_BASE_DIR}/SDL/bin
+)
+
+# Disable building the shared library
+set(SDL_SHARED OFF CACHE BOOL "" FORCE)
+# Enable building the static library
+set(SDL_STATIC ON CACHE BOOL "" FORCE)
+# Disable building SDL3_test
+set(SDL_TEST_LIBRARY OFF CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(glm SDL3)
+
+set_target_properties(glm PROPERTIES FOLDER "Third Party")
+
+# Group SDL Targets under SDL
+get_property(all_targets DIRECTORY ${FETCHCONTENT_BASE_DIR}/SDL/source PROPERTY BUILDSYSTEM_TARGETS)
+foreach(t ${all_targets})
+    set_target_properties(${t} PROPERTIES FOLDER "Third Party/SDL")
+endforeach()
+
+add_library(SDL ALIAS SDL3-static)
--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <SDL3/SDL.h>
 
 int main() 
 {
     std::cout << "Hello World\n";
+
+    SDL_Window* window = nullptr;
+    
     return 0;
 }
